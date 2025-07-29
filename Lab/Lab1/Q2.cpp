7c31bd25a9bb013e3b865e8735d23a51127a61a9
Mohammad Dilshan Alam <cs5240879@iitd.ac.in>
2025-07-29 22:48:38 +0530

    Update Q2.cpp

--- Lab/Lab1/Q2.cpp
+++ Lab/Lab1/Q2.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+int sumDigits(int n){
+    int sum = 0;
+    while( n != 0){
+        sum += n%10;
+        n = n / 10;
+    }
+    return sum;
+}
+
+int main() {
+    std::cout << sumDigits(356855);
+
+    return 0;
+}
 
