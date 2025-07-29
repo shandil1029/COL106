d88d2f472c6f2ec00642270c2c368952516f7c18
Mohammad Dilshan Alam <cs5240879@iitd.ac.in>
2025-07-29 22:49:51 +0530

    Update Q1.cpp

--- Lab/Lab1/Q1.cpp
+++ Lab/Lab1/Q1.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if(n <= 1) {
+            return false; // 0 and 1 are not prime
+        }
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                return false; // Divisible by a number other than 1 and itself
+            }
+        }
+        return true; // No divisors found, so it's prime
+    }
+};
 
