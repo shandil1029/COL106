e6f479a14d9a86d6c2265f0bcb94d3b0a82e6124
Mohammad Dilshan Alam <cs5240879@iitd.ac.in>
2025-07-31 22:15:12 +0530

    Update Q3.cpp

--- Lab/Lab1/Q3.cpp
+++ Lab/Lab1/Q3.cpp
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <iostream>
 
+bool isPerfectSqr(int num){
+    if(num<= 0){
+        return false;
+    }
+    for(int i=1; i*i <= num; i++){
+        if(i*i == num){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{   int num = 16;
+    std:: cout << isPerfectSqr(5);
+
+    return 0;
+}
