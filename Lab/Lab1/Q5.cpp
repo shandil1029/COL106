5039c1c214dda64abb40d4175376bacc06b70eaf
Mohammad Dilshan Alam <cs5240879@iitd.ac.in>
2025-07-31 22:28:44 +0530

    Update Q5.cpp

--- Lab/Lab1/Q5.cpp
+++ Lab/Lab1/Q5.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+
+int countTrailingZero(int num){
+    int countFive = 0;
+    int five = 5;
+    while(true){
+        if(num/five != 0){
+        countFive += num/five;
+        five = five*five;
+        }
+        else{
+            return countFive;
+        }
+    }
+}
+
+int main()
+{   int num = 16;
+    std:: cout << countTrailingZero(15);
+
+    return 0;
+}
