e4fdc1f264a11a7edadd617beac65fd3cc71cf3f
Mohammad Dilshan Alam <cs5240879@iitd.ac.in>
2025-07-31 22:45:39 +0530

    Update Q4.cpp

--- Lab/Lab1/Q4.cpp
+++ Lab/Lab1/Q4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+bool checkPalindrome(int numb){
+    int reverseNum = 0;
+    int num = numb;
+    while(num!=0){
+        reverseNum = reverseNum*10 + num%10;
+        num = num/10;
+    }
+    if(numb == reverseNum){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main()
+{   int num = 16;
+    std:: cout << checkPalindrome(15251);
+
+    return 0;
+}
